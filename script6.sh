75bfd36490994dd8b06cfcac94268e5141590d9d
AidaBustami2 <47245468+AidaBustami2@users.noreply.github.com>
2022-03-06 02:43:30 -0500

    Update script6.sh

--- script6.sh
+++ script6.sh
@@ -1,1 +1,22 @@
 
+#!/bin/bash
+
+if (($(docker ps -a  |grep AIDA2 |wc -l) > 0 ))
+then
+ docker stop $(docker ps -a | grep AIDA2 | awk '{print $1}')
+ docker rm $(docker ps -a | grep AIDA2 | awk '{print $1}')
+fi
+if ((($(docker images |grep img1 |wc -l) > 0 )) && (($(docker ps -a  |grep img1 |wc -l) > 0)) && (($(docker ps -a  |grep AIDA2 |wc -l) > 0)))
+then
+ docker stop $(docker ps -a | grep img1 | awk '{print $1}')
+ docker rm $(docker ps -a | grep img1 | awk '{print $1}')
+ docker rm $(docker ps -a | grep AIDA2 | awk '{print $1}')
+ docker rmi img1
+fi
+
+docker build /root/test2 --rm   -t img1
+docker run -d --privileged=true   --name AIDA2 -e container=docker  -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /run   -v /var/www/html:/var/www/html    -p 7997:8899 img1
+
+
+docker exec  AIDA2 /root/test/isInstalled.sh
+docker exec  AIDA2 /root/test/isRunning.sh
